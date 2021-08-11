f0007816cd68e03c88f15cd9598836d11c590a87
ramil25 <ramilkaharian25@gmail.com>
2021-08-11 20:44:30 +0800

    layunin and isara button done

---
+++ app/src/main/java/com/education/mobileapp/Layunin.kt
@@ -0,0 +1,29 @@
+package com.education.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.github.barteksc.pdfviewer.PDFView
+
+class Layunin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layunin)
+        val actionBar = supportActionBar
+        actionBar!!.setTitle("Layunin")
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        val pdfView = findViewById<PDFView>(R.id.pdf_reader)
+        pdfView.fromAsset("layunin.pdf").load()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+
+                // app icon in action bar clicked; goto parent activity.
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
--- app/src/main/java/com/education/mobileapp/MainActivity.kt
+++ app/src/main/java/com/education/mobileapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.education.mobileapp
 //all importss
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.title="Pangunahing Pamimilian"
         setContentView(R.layout.activity_main)
+    }
+    private val context : Context = this
+    fun openLayunin(view: View) {
+        val i = Intent(context,Layunin::class.java);
+        startActivity(i);
+    }
+
+    fun isara(view: View) {
+        finish()
+        System.exit(0)
     }
 }
