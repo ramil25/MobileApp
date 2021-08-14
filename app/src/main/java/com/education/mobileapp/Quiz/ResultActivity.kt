6cb5a68acb6cc72d0c1f25fc75897f3187a9a566
ramil25 <ramilkaharian25@gmail.com>
2021-08-14 23:00:14 +0800

    replace Main class

--- app/src/main/java/com/education/mobileapp/Quiz/ResultActivity.kt
+++ app/src/main/java/com/education/mobileapp/Quiz/ResultActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.education.mobileapp.MainActivity
 import com.education.mobileapp.R
 import kotlinx.android.synthetic.main.activity_result.*
 
