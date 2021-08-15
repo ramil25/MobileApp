b8d15b850a1a8c0a6d39479219d438fc5b294813
ramil25 <ramilkaharian25@gmail.com>
2021-08-15 14:38:00 +0800

    modified video size landscape and portait mode

--- app/src/main/java/com/education/mobileapp/VideoViewer.kt
+++ app/src/main/java/com/education/mobileapp/VideoViewer.kt
@@ -1,8 +1,11 @@
 package com.education.mobileapp
 
+import android.content.res.Configuration
+import android.graphics.drawable.GradientDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.MediaController
 import android.widget.VideoView
 import java.net.URI
@@ -10,14 +13,32 @@ import java.net.URI
 class VideoViewer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val newConfig: Configuration
         setContentView(R.layout.activity_video_viewer)
         val actionBar =supportActionBar
-        actionBar!!.hide()
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        actionBar!!.title =QuarterList.kwarter_label
         val vv = findViewById<VideoView>(R.id.videoView)
         val mediaController = MediaController(this)
+        var orientation:Int = resources.configuration.orientation
+        newConfig = Configuration(this.resources.configuration)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            actionBar!!.hide()
+        }
         mediaController.setMediaPlayer(vv)
         vv.setMediaController(mediaController)
         vv.setVideoURI(Uri.parse("android.resource://$packageName/raw/vs1"))
         vv.start()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+
+                // app icon in action bar clicked; goto parent activity.
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
